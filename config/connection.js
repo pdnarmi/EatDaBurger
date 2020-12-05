abf971d41fa6ba22a267a4bc898fd9f62a6850f7
Pdnarmi <pdnarmi@ncsu.edu>
2020-12-05 12:04:10 -0500

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,15 @@
-require("dotenv").config();
-const mysql = require("mysql");
+var mysql = require("mysql");
 var connection;
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+{
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: 'root',
+    password: "root",
     database: "burger_db"
-  });
-}
+  })
+};
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
